2bca2304b16f84b2746dfc991c6f4a96717a1752
Andres Erbsen <andreser@mit.edu>
2018-12-06 20:52:35 -0500

    sanity.v

---
+++ src/sanity.v
@@ -0,0 +1,18 @@
+Global Unset Refine Instance Mode.
+Global Set Universe Polymorphism.
+
+(** Work around some counter-intuitive defaults. *)
+
+(** [intuition] means [intuition auto with *].  This is very wrong and
+    fragile and slow.  We make [intuition] mean [intuition auto]. *)
+Tactic Notation "intuition" tactic3(tactic) := intuition tactic.
+Tactic Notation "intuition" := intuition auto.
+
+(** [firstorder] means [firstorder auto with *].  This is very wrong
+    and fragile and slow.  We make [firstorder] mean [firstorder
+    auto]. *)
+Global Set Firstorder Solver auto.
+
+(** A version of [intuition] that allows you to see how the old
+    [intuition] tactic solves the proof. *)
+Ltac debug_intuition := idtac "<infomsg>Warning: debug_intuition should not be used in production code.</infomsg>"; intuition debug auto with *.
