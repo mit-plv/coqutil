eaee702ceaf54178d9a841edaa5b23ecbf7b33f4
Samuel Gruetter <samuel.gruetter@epfl.ch>
2018-12-08 01:14:16 -0500

    lia knows min/max, no need for momega

--- src/Tactics/Tactics.v
+++ src/Tactics/Tactics.v
@@ -67,20 +67,6 @@ Tactic Notation "unique" "pose" "proof" constr(defn) :=
   | _ => pose proof defn
   end.
 
-(* Like "omega" but also knows about min and max *)
-Ltac momega :=
-  repeat match goal with
-         | _: context[  min ?a ?b] |- _ => unique pose proof (Min.min_spec a b)
-         |  |- context[  min ?a ?b]     => unique pose proof (Min.min_spec a b)
-         | _: context[  max ?a ?b] |- _ => unique pose proof (Max.max_spec a b)
-         |  |- context[  max ?a ?b]     => unique pose proof (Max.max_spec a b)
-         | _: context[Z.min ?a ?b] |- _ => unique pose proof (  Z.min_spec a b)
-         |  |- context[Z.min ?a ?b]     => unique pose proof (  Z.min_spec a b)
-         | _: context[Z.max ?a ?b] |- _ => unique pose proof (  Z.max_spec a b)
-         |  |- context[Z.max ?a ?b]     => unique pose proof (  Z.max_spec a b)
-  end;
-  omega.
-
 Ltac hard_assert_is_sort E :=
   let T := type of E in
   lazymatch T with
