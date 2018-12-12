711296b1308230bb9c71360b65dd071af413b3aa
Andres Erbsen <andreser@mit.edu>
2018-12-12 12:57:53 -0500

    Datatypes.String and Map.SortedListString

---
+++ src/Map/SortedListString.v
@@ -0,0 +1,8 @@
+From coqutil Require Import sanity.
+Require coqutil.Map.SortedList coqutil.Datatypes.String.
+Axiom string_strict_order : @SortedList.parameters.strict_order _ String.ltb.
+Definition Build_parameters T := SortedList.parameters.Build_parameters String.string T String.ltb.
+Definition map T := SortedList.map (Build_parameters T) string_strict_order.
+Definition ok T : @Interface.map.ok String.string T (map T).
+  pose proof @SortedList.map_ok (Build_parameters T) as H; eapply H.
+Qed.
