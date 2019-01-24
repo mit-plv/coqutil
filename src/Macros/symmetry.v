855a51a51768057f356518e6b76535c1e877d3ba
Andres Erbsen <andreser@mit.edu>
2019-01-24 00:16:51 -0500

    symmetry! macro for symmetry under binders
    thanks @JasonGross

---
+++ src/Macros/symmetry.v
@@ -0,0 +1,30 @@
+Require Import coqutil.Macros.subst.
+Require Coq.Classes.RelationClasses.
+Ltac exact_sym_under_binders pf :=
+  lazymatch type of pf with
+  | forall x : ?T, _  => let y := fresh x in
+    exact (fun y : T => ltac:(exact_sym_under_binders (pf y)))
+  | let x : ?T := ?v in ?C  => let y := fresh x in
+    exact (let y : T := v in ltac:(exact_sym_under_binders (pf : subst! y for x in C))) (* FIXME: have let-in in the type of the returned lemma *)
+  | _ = _ => exact (Logic.eq_sym pf) (* special-case eq_sym for reduction *)
+  | _     => exact (RelationClasses.symmetry pf)
+  end.
+Notation "symmetry! pf" := (ltac:(exact_sym_under_binders pf))
+                             (at level 10, only parsing).
+
+Goal forall y, exists x, 1 + S y = x.
+  assert (forall y z, z = y+y -> y+y=z) as H by admit.
+  pose proof (symmetry! H).
+
+  assert (forall y, let z := y+y in y+y=z) as HH by admit.
+  pose proof (symmetry! HH).
+
+  assert (forall y, let z := y+y in forall t, t+y+y=t+z) as HHH by admit.
+  pose proof (symmetry! HHH).
+
+  pose proof (symmetry! (fun pf : 1 = 2 => pf)).
+  pose proof ((symmetry! plus_n_Sm) : (forall n m : nat, n + S m = S (n + m))).
+  intros.
+  rewrite (symmetry! plus_n_Sm).
+  exists (S (1 + y)). exact eq_refl.
+Abort.
