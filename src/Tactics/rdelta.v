9a59a6c1ef03ef2ff83185ae0ce081afa0a54898
Andres Erbsen <andreser@mit.edu>
2019-02-20 23:33:25 -0500

    add syntactic_unify_deltavar, rdelta_var, rdelta_const

--- src/Tactics/rdelta.v
+++ src/Tactics/rdelta.v
@@ -1,5 +1,27 @@
 Ltac rdelta x :=
   match constr:(Set) with
-  | _ => let x := eval unfold x in x in x
+  | _ =>
+    (* [unfold] also REFOLDS primitive projections into compat. constants *)
+    let __ := match constr:(Set) with _ => is_var x | _ => is_const x end in
+    let x := eval unfold x in x in
+    rdelta x
+  | _ => x
+  end.
+
+Ltac rdelta_var x :=
+  match constr:(Set) with
+  | _ =>
+    let __ := match constr:(Set) with _ => is_var x end in
+    let x := eval unfold x in x in
+    rdelta_var x
+  | _ => x
+  end.
+
+Ltac rdelta_const x :=
+  match constr:(Set) with
+  | _ =>
+    let __ := match constr:(Set) with _ => is_const x end in
+    let x := eval unfold x in x in
+    rdelta_const x
   | _ => x
   end.
