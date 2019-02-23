7e9e6be136657727aafa8a8d3b35e78b44465810
Andres Erbsen <andreser@mit.edu>
2019-02-23 00:20:56 -0500

    letexists openconstr as ident

--- src/Tactics/letexists.v
+++ src/Tactics/letexists.v
@@ -9,3 +9,13 @@ Tactic Notation "letexists" open_constr(v) :=
   letexists_ v.
 Tactic Notation "letexists" :=
   letexists _.
+
+Ltac letexists_as v x' :=
+  hnf; (* NOTE: jgross says hnf is fragile but idk how else to get ?P *)
+  lazymatch goal with
+  | |- exists x, ?P =>
+    refine (let x' := v in ex_intro (fun x => P) x' _)
+  end.
+
+Tactic Notation "letexists" open_constr(v) "as" ident(x) :=
+  letexists_as v x.
