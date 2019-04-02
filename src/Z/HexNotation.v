7dd95a1fef44ee7501b88f6fcd815c57fdb37837
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-04-02 00:29:42 -0400

    hex_csts_to_dec

--- src/Z/HexNotation.v
+++ src/Z/HexNotation.v
@@ -3,3 +3,11 @@ Require Export Coq.Strings.String.
 Require Coq.Strings.HexString.
 
 Definition Ox(s: string): Z := Z.of_N (HexString.Raw.to_N s N0).
+
+Ltac hex_csts_to_dec :=
+  repeat (let y := match goal with
+                   | _: context [Ox ?a] |- _ => a
+                   | |- context [Ox ?a]      => a
+                   end in
+          let y' := eval cbv in (Ox y) in
+          change (Ox y) with y' in *).
