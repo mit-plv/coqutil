e396357a0a6b368f9ea0870990a3ad1307e2607b
Andres Erbsen <andreser@mit.edu>
2019-04-25 16:52:48 -0400

    Add common widths to Word.Naive

--- src/Word/Naive.v
+++ src/Word/Naive.v
@@ -15,15 +15,8 @@ Section WithWidth.
   Let swrap_value z := wrap_value (z + 2 ^ (width - 1)) - 2 ^ (width - 1).
   Record rep := mk { unsigned : Z ; _unsigned_in_range : wrap_value unsigned = unsigned }.
 
-  Context {width_nonneg : Z.lt 0 width}.
-
-  Lemma wrap_value_wrap_value z : wrap_value (wrap_value z) = wrap_value z.
-    cbv [wrap_value]. rewrite Z.mod_mod; [ reflexivity | ].
-    eapply Z.pow_nonzero; [congruence | blia ].
-  Qed.
-
   Definition wrap (z:Z) : rep :=
-    mk (wrap_value z) (minimize_eq_proof Z.eq_dec (wrap_value_wrap_value z)).
+    mk (wrap_value z) (minimize_eq_proof Z.eq_dec (Zdiv.Zmod_mod z _)).
   Definition signed w := swrap_value (unsigned w).
 
   Unset Universe Minimization ToSet.
@@ -92,6 +85,8 @@ Section WithWidth.
     rewrite Zdiv.Zplus_mod_idemp_l; auto.
   Qed.
 
+  Context (width_nonneg : Z.lt 0 width).
+
   Global Instance ok : word.ok word.
   Proof.
     split; intros;
@@ -105,3 +100,56 @@ Section WithWidth.
 End WithWidth.
 Arguments word : clear implicits.
 Arguments ok : clear implicits.
+
+
+(* NOTE: this can be moved into a separate file to build Properties and the above in parallel *)
+(** [Add Ring] for sizes used in instruction sets of common processors *)
+Require coqutil.Word.Properties.
+Notation word1 := (word 1%Z).
+Instance word1_ok : word.ok word1 := ok 1 eq_refl.
+Add Ring wring1 : (Properties.word.ring_theory (word := word1))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word1)),
+       constants [Properties.word_cst]).
+Notation word8 := (word 8%Z).
+Instance word8_ok : word.ok word8 := ok 8 eq_refl.
+Add Ring wring8 : (Properties.word.ring_theory (word := word8))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word8)),
+       constants [Properties.word_cst]).
+Notation word16 := (word 16%Z).
+Instance word16_ok : word.ok word16 := ok 16 eq_refl.
+Add Ring wring16 : (Properties.word.ring_theory (word := word16))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word16)),
+       constants [Properties.word_cst]).
+Notation word32 := (word 32%Z).
+Instance word32_ok : word.ok word32 := ok 32 eq_refl.
+Add Ring wring32 : (Properties.word.ring_theory (word := word32))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word32)),
+       constants [Properties.word_cst]).
+Notation word64 := (word 64%Z).
+Instance word64_ok : word.ok word64 := ok 64 eq_refl.
+Add Ring wring64 : (Properties.word.ring_theory (word := word64))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word64)),
+       constants [Properties.word_cst]).
+Notation word128 := (word 128%Z).
+Instance word128_ok : word.ok word128 := ok 128 eq_refl.
+Add Ring wring128 : (Properties.word.ring_theory (word := word128))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word128)),
+       constants [Properties.word_cst]).
+Notation word256 := (word 256%Z).
+Instance word256_ok : word.ok word256 := ok 256 eq_refl.
+Add Ring wring256 : (Properties.word.ring_theory (word := word256))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word256)),
+       constants [Properties.word_cst]).
+Notation word512 := (word 512%Z).
+Instance word512_ok : word.ok word512 := ok 512 eq_refl.
+Add Ring wring512 : (Properties.word.ring_theory (word := word512))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (Properties.word.ring_morph (word := word512)),
+       constants [Properties.word_cst]).
