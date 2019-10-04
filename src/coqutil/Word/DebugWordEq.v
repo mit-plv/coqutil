59739c9366190f7e7fac3c5e62d0f6d2a4ae71dc
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-10-03 23:32:12 -0400

    word equality "debugger"
    displays simplified difference between LHS and RHS

---
+++ src/coqutil/Word/DebugWordEq.v
@@ -0,0 +1,42 @@
+From Coq Require Import ZArith.
+Require Coq.setoid_ring.Ring_theory.
+Require Import coqutil.Word.Interface coqutil.Word.Properties.
+
+Section WithWord.
+  Context {width: Z} {word: word.word width} {word_ok: word.ok word}.
+
+  Add Ring wring : (word.ring_theory (word := word))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (word.ring_morph (word := word)),
+       constants [word_cst]).
+
+  Lemma reduce_eq_to_diff0: forall (a b: word), word.sub a b = word.of_Z 0 -> a = b.
+  Proof.
+    intros a b X.
+    replace a with (word.add b (word.sub a b)) by ring.
+    rewrite X.
+    ring.
+  Qed.
+End WithWord.
+
+Ltac debug_word_eq :=
+  apply reduce_eq_to_diff0;
+  match goal with
+  | |- ?x = _ => ring_simplify x
+  end.
+
+Section Demo.
+  Context {width: Z} {word: word.word width} {word_ok: word.ok word}.
+
+  Add Ring wring' : (word.ring_theory (word := word))
+      (preprocess [autorewrite with rew_word_morphism],
+       morphism (word.ring_morph (word := word)),
+       constants [word_cst]).
+
+  Example slightly_off: forall (a b c: word),
+      word.add a (word.add b c) = word.add (word.add b (word.of_Z 1)) (word.add a c).
+  Proof.
+    intros.
+    debug_word_eq.
+  Abort.
+End Demo.
