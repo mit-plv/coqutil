5b6830f2912e58fc4b536f9912c93f8b0f2bceb0
Samuel Gruetter <samuel.gruetter@epfl.ch>
2019-10-29 19:06:13 -0400

    another slow map goal

--- src/coqutil/Map/SlowGoals.v
+++ src/coqutil/Map/SlowGoals.v
@@ -61,4 +61,42 @@ Proof.
      it returned faster. *)
 Abort.
 
+Goal
+  forall (S : Set) (T : Type) (M0 : map.map S T),
+  map.ok M0 ->
+  forall b : S -> S -> bool,
+  EqDecider b ->
+  forall (l initialL_regs middle_regs lH' middle_regs0 finalRegsH middle_regs1 finalRegsH0 : M0)
+    (ks0 ks : S -> Prop) (k k2 k6 k1 k7 k5 k4 k3 k0 x : S) (p_sp0 : T),
+  k7 = k6 ->
+  k5 = k6 ->
+  (forall (x0 : S) (w : T), map.get l x0 = Some w -> map.get initialL_regs x0 = Some w) ->
+  map.get initialL_regs k2 = Some p_sp0 ->
+  (forall x0 : S,
+   x0 \in union ks (singleton_set k) \/ map.get initialL_regs x0 = map.get middle_regs x0) ->
+  (forall (x0 : S) (w : T), map.get lH' x0 = Some w -> map.get middle_regs x0 = Some w) ->
+  map.get middle_regs k2 = Some p_sp0 ->
+  k4 = k1 ->
+  (forall x0 : S,
+   x0 \in union ks0 (singleton_set k) \/ map.get middle_regs x0 = map.get middle_regs0 x0) ->
+  (forall (x0 : S) (w : T), map.get finalRegsH x0 = Some w -> map.get middle_regs0 x0 = Some w) ->
+  map.get middle_regs0 k2 = Some p_sp0 ->
+  k3 = k1 ->
+  (forall x0 : S,
+   x0 \in union (union ks ks0) (singleton_set k) \/
+   map.get middle_regs0 x0 = map.get middle_regs1 x0) ->
+  (forall (x0 : S) (w : T), map.get finalRegsH0 x0 = Some w -> map.get middle_regs1 x0 = Some w) ->
+  map.get middle_regs1 k2 = Some p_sp0 ->
+  k0 = k1 ->
+  x \in union (union ks ks0) (singleton_set k) \/
+  map.get initialL_regs x = map.get middle_regs1 x.
+Proof.
+  intros.
+  subst.
+  (*
+  Time map_solver H.
+  Finished transaction in 113.098 secs (112.608u,0.056s) (successful)
+  *)
+Abort.
+
 (*Goal True. idtac "End of SlowGoals.v". Abort.*)
