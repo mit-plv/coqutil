17031010956f96362b918c866eb3f3c5f7c3a08b
Samuel Gruetter <samuel.gruetter@epfl.ch>
2020-02-04 20:48:53 -0500

    Coq.Init.Byte lemmas & use it in LittleEndian.v instead of "word 8"

---
+++ src/coqutil/Byte.v
@@ -0,0 +1,106 @@
+Require Coq.Init.Byte Coq.Strings.Byte.
+Require Import Coq.ZArith.ZArith.
+
+Local Open Scope Z_scope.
+
+Notation byte := (Coq.Init.Byte.byte: Type). (* to avoid that some universes are forced to "Set" *)
+
+Module byte.
+  Definition unsigned(b: byte): Z := Z.of_N (Byte.to_N b).
+
+  Definition wrap(z: Z): Z := z mod 2 ^ 8.
+
+  Lemma Byte_of_N_of_mod_not_None: forall z, Byte.of_N (Z.to_N (wrap z)) <> None.
+  Proof.
+    intros z C.
+    apply Byte.of_N_None_iff in C.
+    pose proof (Z.mod_pos_bound z 256 eq_refl) as P.
+    destruct P as [P1 P2].
+    apply Z2N.inj_lt in P2. 3: cbv; discriminate. 2: assumption.
+    apply N.lt_le_pred in P2.
+    simpl in P2.
+    pose proof (N.lt_le_trans _ _ _ C P2) as P3.
+    cbv in P3.
+    discriminate.
+  Qed.
+
+  Definition of_Z(z: Z): byte :=
+    let r := Byte.of_N (Z.to_N (wrap z)) in
+    match r as o return (r = o -> byte) with
+    | Some b => fun _ => b
+    | None => fun E => False_rect byte (Byte_of_N_of_mod_not_None z E)
+    end eq_refl.
+
+  Lemma unsigned_of_Z: forall z, unsigned (of_Z z) = wrap z.
+  Proof.
+    intros. unfold unsigned.
+    pose proof (Z.mod_pos_bound z (2^8) eq_refl) as P.
+    rewrite <- (Z2N.id (wrap z)) by (destruct P; assumption).
+    f_equal.
+    destruct (Byte.of_N (Z.to_N (wrap z))) eqn: E. 2: {
+      apply Byte_of_N_of_mod_not_None in E. contradiction.
+    }
+    erewrite <- (Byte.to_of_N (Z.to_N (wrap z))). 2: exact E.
+    f_equal.
+    unfold of_Z.
+    remember (fun E0 : Byte.of_N (Z.to_N (wrap z)) = None => False_rect byte (Byte_of_N_of_mod_not_None z E0))
+      as F; clear HeqF.
+    revert E F.
+    generalize (Byte.of_N (Z.to_N (wrap z))).
+    clear z P.
+    intros o E.
+    rewrite E.
+    intros.
+    reflexivity.
+  Qed.
+
+  Lemma of_bits_inj: forall bs1 bs2, Byte.of_bits bs1 = Byte.of_bits bs2 -> bs1 = bs2.
+  Proof.
+    intros.
+    rewrite <- (Byte.to_bits_of_bits bs1) in *.
+    rewrite <- (Byte.to_bits_of_bits bs2) in *.
+    do 2 rewrite Byte.of_bits_to_bits in H.
+    f_equal.
+    assumption.
+  Qed.
+
+  Lemma to_bits_inj: forall b1 b2, Byte.to_bits b1 = Byte.to_bits b2 -> b1 = b2.
+  Proof.
+    intros.
+    rewrite <- (Byte.of_bits_to_bits b1) in *.
+    rewrite <- (Byte.of_bits_to_bits b2) in *.
+    do 2 rewrite Byte.to_bits_of_bits in H.
+    f_equal.
+    assumption.
+  Qed.
+
+  Lemma to_N_inj: forall b1 b2, Byte.to_N b1 = Byte.to_N b2 -> b1 = b2.
+  Proof.
+    intros.
+    enough (Some b1 = Some b2). 1: congruence.
+    rewrite <- (Byte.of_to_N b1).
+    rewrite <- (Byte.of_to_N b2).
+    f_equal.
+    assumption.
+  Qed.
+
+  Lemma unsigned_inj: forall b1 b2, unsigned b1 = unsigned b2 -> b1 = b2.
+  Proof.
+    unfold unsigned. intros.
+    apply N2Z.inj in H.
+    apply to_N_inj in H.
+    assumption.
+  Qed.
+
+  Lemma wrap_unsigned: forall x, wrap (unsigned x) = unsigned x.
+  Proof.
+    intros. unfold wrap, unsigned.
+    apply Z.mod_small. split.
+    - apply N2Z.is_nonneg.
+    - change (2 ^ 8) with (Z.of_N (N.succ 255)).
+      apply N2Z.inj_lt.
+      apply N.lt_succ_r.
+      apply (Byte.to_N_bounded x).
+  Qed.
+
+End byte.
--- src/coqutil/Word/LittleEndian.v
+++ src/coqutil/Word/LittleEndian.v
@@ -3,40 +3,40 @@ Require Import coqutil.Z.Lia.
 Require Import coqutil.Word.Interface coqutil.Datatypes.HList coqutil.Datatypes.PrimitivePair.
 Require Import coqutil.Word.Properties.
 Require Import coqutil.Z.bitblast.
+Require Import coqutil.Byte.
 Local Set Universe Polymorphism.
 
 Local Open Scope Z_scope.
 
 Section LittleEndian.
-  Context {byte: word 8}.
 
   Fixpoint combine (n : nat) : forall (bs : tuple byte n), Z :=
     match n with
     | O => fun _ => 0
-    | S n => fun bs => Z.lor (word.unsigned (pair._1 bs))
+    | S n => fun bs => Z.lor (byte.unsigned (pair._1 bs))
                              (Z.shiftl (combine n (pair._2 bs)) 8)
     end.
 
   Fixpoint split (n : nat) (w : Z) : tuple byte n :=
     match n with
     | O => tt
-    | S n => pair.mk (word.of_Z w) (split n (Z.shiftr w 8))
+    | S n => pair.mk (byte.of_Z w) (split n (Z.shiftr w 8))
     end.
 
-  Lemma combine_split {ok : word.ok byte} (n : nat) (z : Z) :
+  Lemma combine_split (n : nat) (z : Z) :
     combine n (split n z) = z mod 2 ^ (Z.of_nat n * 8).
   Proof.
     revert z; induction n.
     - cbn. intros. rewrite Z.mod_1_r. trivial.
     - cbn [split combine PrimitivePair.pair._1 PrimitivePair.pair._2]; intros.
       erewrite IHn; clear IHn.
-      rewrite word.unsigned_of_Z, Nat2Z.inj_succ, Z.mul_succ_l by blia.
-      unfold word.wrap.
+      rewrite byte.unsigned_of_Z, Nat2Z.inj_succ, Z.mul_succ_l by blia.
+      unfold byte.wrap.
       rewrite <-! Z.land_ones by blia.
       Z.bitblast.
   Qed.
 
-  Lemma split_combine {ok : word.ok byte} (n: nat) bs :
+  Lemma split_combine (n: nat) bs :
     split n (combine n bs) = bs.
   Proof.
     revert bs; induction n.
@@ -44,15 +44,15 @@ Section LittleEndian.
     - destruct bs; cbn [split combine PrimitivePair.pair._1 PrimitivePair.pair._2]; intros.
       specialize (IHn _2).
       f_equal.
-      { eapply Properties.word.unsigned_inj.
-        rewrite word.unsigned_of_Z, <-Properties.word.wrap_unsigned; cbv [word.wrap].
+      { eapply byte.unsigned_inj.
+        rewrite byte.unsigned_of_Z, <-byte.wrap_unsigned; cbv [byte.wrap].
         Z.bitblast; cbn; subst.
         rewrite (Z.testbit_neg_r _ (i-8)) by bomega.
         Z.bitblast_core. }
       { rewrite <-IHn.
         rewrite combine_split.
         f_equal.
-        rewrite <-word.wrap_unsigned.
+        rewrite <-byte.wrap_unsigned; cbv [byte.wrap].
         Z.bitblast; subst; cbn.
         rewrite <-IHn.
         rewrite combine_split.
