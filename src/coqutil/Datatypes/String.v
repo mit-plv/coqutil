af182d4224f6fe5b4b2c2e2b7045b94e72f6ad74
Jason Gross <jgross@mit.edu>
2020-03-25 19:50:27 -0400

    Remove a strict_order axiom

--- src/coqutil/Datatypes/String.v
+++ src/coqutil/Datatypes/String.v
@@ -5,6 +5,21 @@ Require Export Coq.Strings.String.
 
 Module Ascii.
   Definition ltb (c d : Ascii.ascii) : bool := BinNatDef.N.ltb (Ascii.N_of_ascii c) (Ascii.N_of_ascii d).
+
+  Lemma ltb_antirefl : forall k, ltb k k = false.
+  Proof. cbv [ltb]; intro; apply BinNat.N.ltb_irrefl. Qed.
+
+  Lemma ltb_trans : forall k1 k2 k3, ltb k1 k2 = true -> ltb k2 k3 = true -> ltb k1 k3 = true.
+  Proof.
+    cbv [ltb]; intros *; rewrite !BinNat.N.ltb_lt; intros; etransitivity; eassumption.
+  Qed.
+
+  Lemma ltb_total : forall k1 k2, ltb k1 k2 = false -> ltb k2 k1 = false -> k1 = k2.
+  Proof.
+    cbv [ltb]; intros k1 k2; rewrite !BinNat.N.ltb_ge; intros.
+    rewrite <- (Ascii.ascii_N_embedding k1), <- (Ascii.ascii_N_embedding k2); apply f_equal.
+    apply BinNat.N.le_antisymm; assumption.
+  Qed.
 End Ascii.
 
 Fixpoint ltb (a b : string) : bool :=
@@ -16,6 +31,38 @@ Fixpoint ltb (a b : string) : bool :=
       else Ascii.ltb x y
     | _, _ => false
   end.
+
+Local Ltac strict_order_t :=
+  cbn [ltb];
+  repeat first [ reflexivity
+               | congruence
+               | progress subst
+               | rewrite Ascii.eqb_refl in *
+               | rewrite Ascii.ltb_antirefl in *
+               | match goal with
+                 | [ H : ?x = false |- context[?x] ] => rewrite H
+                 | [ |- context[if Ascii.eqb ?x ?y then _ else _] ] => destruct (Ascii.eqb_spec x y)
+                 | [ H1 : Ascii.ltb ?x ?y = true, H2 : Ascii.ltb ?y ?x = true |- _ ]
+                   => exfalso;
+                      clear -H1 H2;
+                      assert (Ascii.ltb x x = true) by (eapply Ascii.ltb_trans; eassumption);
+                      clear H1 H2
+                 | [ H1 : Ascii.ltb ?x ?y = false, H2 : Ascii.ltb ?y ?x = false |- _ ]
+                   => assert (x = y) by (apply Ascii.ltb_total; assumption);
+                      clear H1 H2
+                 | [ |- String _ _ = String _ _ ] => apply f_equal2
+                 end
+               | progress intros
+               | solve [ eauto using Ascii.ltb_trans with nocore ] ].
+
+Lemma ltb_antirefl : forall k, ltb k k = false.
+Proof. induction k; strict_order_t. Qed.
+
+Lemma ltb_trans : forall k1 k2 k3, ltb k1 k2 = true -> ltb k2 k3 = true -> ltb k1 k3 = true.
+Proof. induction k1, k2, k3; strict_order_t. Qed.
+
+Lemma ltb_total : forall k1 k2, ltb k1 k2 = false -> ltb k2 k1 = false -> k1 = k2.
+Proof. induction k1, k2; strict_order_t. Qed.
 
 Instance eqb_spec: EqDecider eqb.
 Proof.
--- src/coqutil/Map/SortedListString.v
+++ src/coqutil/Map/SortedListString.v
@@ -1,7 +1,10 @@
 From coqutil Require Import sanity.
 Require coqutil.Map.SortedList coqutil.Datatypes.String.
-Local Axiom TODO_andres: False.
-Lemma string_strict_order: @SortedList.parameters.strict_order _ String.ltb. Proof. case TODO_andres. Qed.
+(* Make this an instance so we can use single-curly-braces so we don't need to qualify field-names with [SortedList.parameters.] *)
+Local Instance string_strict_order: @SortedList.parameters.strict_order _ String.ltb
+  := { ltb_antirefl := String.ltb_antirefl
+       ; ltb_trans := String.ltb_trans
+       ; ltb_total := String.ltb_total }.
 Definition Build_parameters T := SortedList.parameters.Build_parameters String.string T String.ltb.
 Definition map T := SortedList.map (Build_parameters T) string_strict_order.
 Definition ok T : @Interface.map.ok String.string T (map T).
