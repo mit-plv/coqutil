a03b783954c3986f6c791ee2db984ef9b9b0e2f7
Samuel Gruetter <samuel.gruetter@epfl.ch>
2021-03-11 22:44:54 -0500

    assert_fails is now correct in stdlib

--- src/coqutil/sanity.v
+++ src/coqutil/sanity.v
@@ -16,18 +16,3 @@ Global Set Firstorder Solver auto.
 (** A version of [intuition] that allows you to see how the old
     [intuition] tactic solves the proof. *)
 Ltac debug_intuition := idtac "<infomsg>Warning: debug_intuition should not be used in production code.</infomsg>"; intuition debug auto with *.
-
-(** [assert_fails] and [assert_succeeds] should never solve the goal.
-    This redefinition fixes both of them, and the notations of the standard library can
-    be reused.
-    https://github.com/coq/coq/issues/9114 *)
-Ltac assert_fails tac ::=
-  tryif (cut True; [ intros _; tac | ]) then fail 0 tac "succeeds" else idtac.
-
-Goal 1 = 1.
-  assert_succeeds reflexivity.
-  assert_fails (exact I).
-  Fail assert_succeeds (exact I).
-  Fail assert_fails reflexivity.
-  idtac.
-Abort.
