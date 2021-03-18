ddbb6f98923ab3b1fec1de7a5441911446656f2b
Samuel Gruetter <samuel.gruetter@epfl.ch>
2021-03-18 15:06:20 -0400

    traversal-based Ltac1 safe_simpl (not going into types or under binders)
    revert-and-re-intro can have undesired effects on section variables,
    eg https://github.com/coq/coq/issues/13953

--- src/coqutil/Tactics/SafeSimpl.v
+++ src/coqutil/Tactics/SafeSimpl.v
@@ -15,18 +15,78 @@ Require Import coqutil.Tactics.Tactics.
 
 Class SafeSimpl{T: Type}(head: T)(arity: nat) := {}.
 
-Ltac safe_simpl :=
-  repeat so fun hyporgoal => match hyporgoal with
-         | context[?h] =>
-           assert_succeeds (is_const h);
-           let c := constr:(_ : SafeSimpl h _) in
-           lazymatch type of c with
-           (* once COQBUG https://github.com/coq/coq/issues/13933 is fixed,
-              we might be able to generalize to any arity *)
-           | SafeSimpl _ 1 => progress simpl (h _) in *
-           | SafeSimpl _ 2 => progress simpl (h _ _) in *
-           | SafeSimpl _ 3 => progress simpl (h _ _ _) in *
-           | SafeSimpl _ 0 => fail 10000 "'SafeSimpl _ 0' instances are useless"
-           | SafeSimpl _ ?n => fail 10000 "The number" n "is too big"
+Ltac simpl_arity h :=
+  let c := constr:(_ : SafeSimpl h _) in
+  lazymatch type of c with
+  | SafeSimpl _ ?n => n
+  end.
+
+Ltac head e :=
+  lazymatch e with
+  | ?a _ => head a
+  | _ => e
+  end.
+
+Ltac app_arity e :=
+  lazymatch e with
+  | ?a _ => let r := app_arity a in constr:(S r)
+  | _ => constr:(O)
+  end.
+
+Ltac safe_simpl_term t :=
+  let h := head t in
+  let n := app_arity t in
+  lazymatch n with
+  | O => lazymatch t with
+         | if ?x then ?a else ?b =>
+           let x' := safe_simpl_term x in
+           let a' := safe_simpl_term a in
+           let b' := safe_simpl_term b in
+           constr:(if x' then a' else b')
+         | match ?x with _ => _ end =>
+           lazymatch t with
+           | context C[x] =>
+             let x' := safe_simpl_term x in
+             context C[x']
            end
-         end.
+         (* TODO here we should also treat match branches, fun, fix, and also recurse into the types *)
+         | _ => t
+         end
+  | _ => let p := match constr:(Set) with
+                  | _ => let __ := match constr:(Set) with _ => is_const h end in
+                         let a := simpl_arity h in
+                         let b := eval cbv in (Nat.leb a n) in
+                         lazymatch b with
+                         | true => let m := eval cbv in (Nat.sub n a) in constr:((true, m))
+                         | false => constr:((false, n))
+                         end
+                  | _ => constr:((false, n))
+                  end in
+         lazymatch p with
+         | (?b, ?m) => safe_simpl_n_args b m t
+         end
+  end
+(* safe_simpl the n right-most arguments of term t, and then if do_simpl_head,
+   run simpl on the remaining head, or else return the remaining head unchanged *)
+with safe_simpl_n_args do_simpl_head n t :=
+  lazymatch n with
+  | O => lazymatch do_simpl_head with
+         | true => eval simpl in t
+         | false => t
+         end
+  | S ?m => lazymatch t with
+            | ?a ?b => let b' := safe_simpl_term b in
+                       let a' := safe_simpl_n_args do_simpl_head m a in
+                       constr:(a' b')
+            | _ => fail 10000 "bug: expected application, found" t
+            end
+  end.
+
+Ltac safe_simpl :=
+  match goal with
+  | |- ?G => let G' := safe_simpl_term G in change G'
+  end;
+  repeat lazymatch goal with
+         | H: ?T |- _ => let T' := safe_simpl_term T in change T' in H; revert H
+         end;
+  intros.
