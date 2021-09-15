eabff516d4c57c3c46aab1ba1966f43ae88e8239
Jason Gross <jgross@mit.edu>
2021-09-14 18:28:35 -0700

    Add GitHub Actions CI
    We test back to 8.9 because that's what we want to ideally support for
    fiat-crypto.

---
+++ etc/ci/github-actions-make.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -x
+
+if [ ! -z "${TOUCH}" ]; then
+    make -j2 -t ${TOUCH} || exit $?
+fi
+
+OUTPUT_SYNC=""
+if make -h 2>/dev/null | grep -q output-sync; then
+    OUTPUT_SYNC="--output-sync"
+else
+    echo "WARNING: make does not support --output-sync"
+fi
+
+reportify=" --errors"
+
+if [ "$1" == "--warnings" ]; then
+    reportify+=" $1"
+    shift
+fi
+if [ ! -z "${reportify}" ]; then
+    reportify="COQC='$(pwd)/etc/coq-scripts/github/reportify-coq.sh'${reportify} ${COQBIN}coqc"
+fi
+
+rm -f finished.ok
+(make "$@" ${OUTPUT_SYNC} TIMED=1 TIMING=1 "${reportify}" 2>&1 && touch finished.ok) | tee -a time-of-build.log
+python "./etc/coq-scripts/timing/make-one-time-file.py" "time-of-build.log" "time-of-build-pretty.log" || exit $?
+
+git status
+git diff
+
+cat time-of-build-pretty.log
+make "$@" TIMED=1 TIMING=1 || exit $?
+
+unameOut="$(uname -s)"
+if [ ! -z "$(git diff)" ]; then
+    git submodule foreach --recursive git diff
+    git submodule foreach --recursive git status
+    git diff
+    if [ "${ALLOW_DIFF}" != "1" ]; then
+        exit 1
+    fi
+fi
