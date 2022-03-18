fd6324e373064151abfa0122b215a3e9db8bc15b
Samuel Gruetter <saemi472@gmail.com>
2022-03-18 01:07:30 -0400

    remove Ltac2 term classification tactics, because they are now
    part of Coq, and add mkApp helpers

--- src/coqutil/Ltac2Lib/Constr.v
+++ src/coqutil/Ltac2Lib/Constr.v
@@ -1,51 +1,32 @@
-(* TODO delete this file once these tactics are in the standard library *)
-
 Require Import Ltac2.Ltac2 Ltac2.Constr.
 
-Ltac2 is_evar(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Evar _ _ => true
-  | _ => false
-  end.
+Ltac2 mkApp(f: constr)(args: constr array) :=
+  Constr.Unsafe.make (Constr.Unsafe.App f args).
 
-Ltac2 is_var(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Var _ => true
-  | _ => false
-  end.
-
-Ltac2 is_fix(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Fix _ _ _ _ => true
-  | _ => false
-  end.
-
-Ltac2 is_cofix(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.CoFix _ _ _ => true
-  | _ => false
-  end.
-
-Ltac2 is_ind(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Ind _ _ => true
-  | _ => false
-  end.
-
-Ltac2 is_constructor(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Constructor _ _ => true
-  | _ => false
-  end.
-
-Ltac2 is_proj(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Proj _ _ => true
-  | _ => false
-  end.
-
-Ltac2 is_const(c: constr) :=
-  match Unsafe.kind c with
-  | Unsafe.Constant _ _ => true
-  | _ => false
-  end.
+(* If we had array literals, these boilerplate functions would not be needed
+   https://github.com/coq/coq/issues/13976 *)
+Ltac2 mkApp1(f: constr)(a0: constr) :=
+  let args := Array.make 1 a0 in
+  Constr.Unsafe.make (Constr.Unsafe.App f args).
+Ltac2 mkApp2(f: constr)(a0: constr)(a1: constr) :=
+  let args := Array.make 2 a0 in
+  Array.set args 1 a1;
+  Constr.Unsafe.make (Constr.Unsafe.App f args).
+Ltac2 mkApp3(f: constr)(a0: constr)(a1: constr)(a2: constr) :=
+  let args := Array.make 3 a0 in
+  Array.set args 1 a1;
+  Array.set args 2 a2;
+  Constr.Unsafe.make (Constr.Unsafe.App f args).
+Ltac2 mkApp4(f: constr)(a0: constr)(a1: constr)(a2: constr)(a3: constr) :=
+  let args := Array.make 4 a0 in
+  Array.set args 1 a1;
+  Array.set args 2 a2;
+  Array.set args 3 a3;
+  Constr.Unsafe.make (Constr.Unsafe.App f args).
+Ltac2 mkApp5(f: constr)(a0: constr)(a1: constr)(a2: constr)(a3: constr)(a4: constr) :=
+  let args := Array.make 5 a0 in
+  Array.set args 1 a1;
+  Array.set args 2 a2;
+  Array.set args 3 a3;
+  Array.set args 4 a4;
+  Constr.Unsafe.make (Constr.Unsafe.App f args).
