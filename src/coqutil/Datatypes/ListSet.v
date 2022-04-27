4d109fc0a342cb235c2e99b76104c5bf0475e418
Pratap Singh <pratapsingh1729@gmail.com>
2022-04-27 16:05:40 -0400

    add list_diff_length (#64)

--- src/coqutil/Datatypes/ListSet.v
+++ src/coqutil/Datatypes/ListSet.v
@@ -291,4 +291,16 @@ Section ListSetProofs. Local Set Default Proof Using "All".
         * specialize IHl1 with (1 := H). destruct IHl1. auto.
   Qed.
 
+  Lemma list_diff_length: forall (l1 l2: list E),
+      length (list_diff eeq l1 l2) <= length l1. 
+  Proof.
+    intros. induction l1. 
+    - cbn. rewrite list_diff_empty_l. auto.
+    - cbn. rewrite list_diff_cons.
+      destruct (find (eeq a) l2) eqn:F.
+      + auto.
+      + cbn. blia. 
+  Qed. 
+
+
 End ListSetProofs.
