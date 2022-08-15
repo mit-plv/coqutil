21b0ec1d154ba535d8675c0212e26262104e77c4
Samuel Gruetter <saemi472@gmail.com>
2022-08-15 17:53:41 -0400

    map.of_olist_Z

---
+++ src/coqutil/Map/OfOptionListZ.v
@@ -0,0 +1,18 @@
+Require Import ZArith.
+Require Import Coq.Lists.List coqutil.Datatypes.List coqutil.Datatypes.ZList.
+Require Import coqutil.Map.Interface coqutil.Map.OfFunc.
+Import Interface.map MapKeys.map OfFunc.map.
+
+Module map.
+  Section __. Local Set Default Proof Using "All".
+    Context [value : Type] {map : map Z value} {ok : map.ok map}.
+
+    (* TODO maybe use wrap-around addition on Z instead of Z.add ? *)
+
+    Definition of_olist_Z (xs : list (option value)) : map :=
+      map.of_func (List.get xs) (List.map Z.of_nat (seq O (length xs))).
+
+    Definition of_olist_Z_at (a : Z) (xs : list (option value)) : map :=
+      map_keys (Z.add a) (of_olist_Z xs).
+  End __.
+End map.
