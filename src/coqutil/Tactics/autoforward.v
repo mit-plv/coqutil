96c3b6e013a61f87fb8e245edca83fd67f17bbc8
Samuel Gruetter <saemi472@gmail.com>
2022-08-22 18:37:06 -0400

    don't autoforward with A->A

--- src/coqutil/Tactics/autoforward.v
+++ src/coqutil/Tactics/autoforward.v
@@ -7,8 +7,9 @@ Ltac autoforward_in db H :=
   rename H into tmp;
   let A := type of tmp in
   pose proof ((ltac:(typeclasses eauto with db) : autoforward A _) tmp) as H;
-  move H after tmp;
-  clear tmp.
+  let B := type of H in
+  tryif constr_eq A B then fail "autoforward typeclass search returned identity implication"
+  else (move H after tmp; clear tmp).
 
 Tactic Notation "autoforward" "with" ident(db) "in" hyp(H) :=
   autoforward_in db H.
