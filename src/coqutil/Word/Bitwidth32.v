4b3074b06fa3722ed5b59d0afdc5fec99207fd71
Samuel Gruetter <saemi472@gmail.com>
2023-11-20 13:03:49 -0500

    make Bitwidth instances `#[export]` instead of `#[global]`

--- src/coqutil/Word/Bitwidth32.v
+++ src/coqutil/Word/Bitwidth32.v
@@ -1,6 +1,6 @@
 Require Import Coq.ZArith.ZArith.
 Require Export coqutil.Word.Bitwidth.
 
-#[global] Instance BW32: Bitwidth 32 := {
+#[export] Instance BW32: Bitwidth 32 := {
   width_cases := or_introl eq_refl
 }.
--- src/coqutil/Word/Bitwidth64.v
+++ src/coqutil/Word/Bitwidth64.v
@@ -1,6 +1,6 @@
 Require Import Coq.ZArith.ZArith.
 Require Export coqutil.Word.Bitwidth.
 
-#[global] Instance BW64: Bitwidth 64 := {
+#[export] Instance BW64: Bitwidth 64 := {
   width_cases := or_intror eq_refl
 }.
