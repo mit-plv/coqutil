624ce7eb0cf4701dcecd00826a445494d2f84252
dijamner <dijamner@me.com>
2025-03-05 17:38:38 -0500

    Move DecidableEq back to pyrosome since it's not advised for use

--- src/coqutil/Eqb.v
+++ src/coqutil/Eqb.v
@@ -17,41 +17,8 @@ Section __.
   Definition eqb_spec {Impl : Eqb} {Pf : @Eqb_ok Impl} : forall a b, if eqb a b then a = b else a <> b := Pf.
   Existing Class Eqb_ok.
 
-  
-  (* TODO: is the no-record version worth it here to avoid firstorder trouble? *)
-  Class DecidableEq :=
-    {
-      dec : forall (s1 s2 : A), {s1 = s2} + {s1 <> s2}
-    }.
-
-  (* Note: do not export. Should be declared as an instance only when no boolean implementation yet exists since it will likely have poor performance.   
-   *)
-  Instance eqb_from_decidable `{DecidableEq} : Eqb :=
-    fun a b => if dec a b then true else false.
-
-  
-  Instance eqb_from_decidable_ok `{DecidableEq} : Eqb_ok.
-  Proof.
-    intros a b.
-    unfold eqb, eqb_from_decidable.
-    destruct (dec a b); auto.
-  Qed.
-
 
   Context `{Eqb_ok}.
-  
-  (* Note: do not export. This instance should not be expected to compute, since it depends on a lemma
-     that is probably defined with Qed. To emphasize this, we define this instance with Qed as well.
-     Thus, it should be used with caution.
-   *)
-  Instance decidable_from_eqb_ok : DecidableEq.
-  Proof.
-    constructor; intros.
-    specialize (H0 s1 s2).
-    revert H0.
-    case_match;
-      intros; subst; eauto.
-  Qed.
 
 
   Lemma eqb_prop_iff
@@ -98,7 +65,6 @@ End __.
 Arguments eqb {A}%type_scope {Impl} _ _.
 Arguments Eqb_ok {A}%type_scope H.
 Arguments eqb_spec {A}%type_scope {Impl Pf} a b.
-Arguments dec {A}%type_scope {DecidableEq} s1 s2.
    
 #[export] Hint Rewrite eqb_prop_iff using solve[typeclasses eauto] : bool.
 #[export] Hint Rewrite eqb_refl_true using solve[typeclasses eauto] : bool.
