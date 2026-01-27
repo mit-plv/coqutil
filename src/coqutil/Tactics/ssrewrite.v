4d3dabee096e8f5b4dbf5025238e28c8286c1ec5
Andres Erbsen <andres-github@andres.systems>
2026-01-27 14:46:50 -0800

    Update ssrewrite.v to not shadow normal rewrite

--- src/coqutil/Tactics/ssrewrite.v
+++ src/coqutil/Tactics/ssrewrite.v
@@ -3,11 +3,11 @@ Require Import coqutil.Macros.symmetry.
 
 Import ssreflect.
 
-Tactic Notation "rewrite" constr(pf) := rewrite pf.
-Tactic Notation "rewrite" "<-" constr(pf) := rewrite (symmetry! pf).
-Tactic Notation "rewrite" "!" constr(pf) := rewrite !pf.
-Tactic Notation "rewrite" "<-" "!" constr(pf) := rewrite !(symmetry! pf).
-Tactic Notation "rewrite" "?" constr(pf) := rewrite ? pf.
-Tactic Notation "rewrite" "<-" "?" constr(pf) := rewrite ?(symmetry! pf).
+Tactic Notation "ssrewrite" open_constr(pf) := rewrite pf.
+Tactic Notation "ssrewrite" "<-" open_constr(pf) := rewrite (symmetry! pf).
+Tactic Notation "ssrewrite" "!" open_constr(pf) := rewrite !pf.
+Tactic Notation "ssrewrite" "<-" "!" constr(pf) := rewrite !(symmetry! pf).
+Tactic Notation "ssrewrite" "?" open_constr(pf) := rewrite ? pf.
+Tactic Notation "ssrewrite" "<-" "?" open_constr(pf) := rewrite ?(symmetry! pf).
 
-Ltac rewrite pf := rewrite pf.
+Ltac ssrewrite pf := rewrite pf.
