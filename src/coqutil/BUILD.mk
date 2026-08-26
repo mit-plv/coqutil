12e3c24f7233b4c9133acd63a5f1bab96ce60e26
Miriam Polzer <mpolzer@google.com>
2026-08-26 10:34:36 +0200

    Allow Rocq 9.0 minimum, support 9.1 and 9.2, update CI, opam, and warnings

--- src/coqutil/BUILD.mk
+++ src/coqutil/BUILD.mk
@@ -3,7 +3,7 @@ COQUTIL_VFILES := $(call rwildcard,$(COQUTIL_DIR),*.v)
 COQUTIL_COQDEPFLAGS := -Q $(COQUTIL_DIR) $(notdir $(COQUTIL_DIR))
 COQUTIL_REQUIREFLAGS := -Q $(O)/$(COQUTIL_DIR) $(notdir $(COQUTIL_DIR))
 
-COQUTIL_COQFLAGS := $(COQUTIL_REQUIREFLAGS) -w -deprecated-from-Coq,-deprecated-since-9.0,-deprecated-since-8.20
+COQUTIL_COQFLAGS := $(COQUTIL_REQUIREFLAGS) -w -deprecated-from-Coq,-deprecated-since-9.2,-deprecated-since-9.1,-deprecated-since-9.0
 $(O)/$(COQUTIL_DIR)/%.vo: private COQFLAGS += $(COQUTIL_COQFLAGS)
 $(O)/$(COQUTIL_DIR)/%.vos: private COQFLAGS += $(COQUTIL_COQFLAGS)
 $(O)/$(COQUTIL_DIR)/%.vok: private COQFLAGS += $(COQUTIL_COQFLAGS)
